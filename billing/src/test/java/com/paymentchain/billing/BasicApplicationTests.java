27427456f5c2ea6aa535c86c57b49d3addc68f1b
udemy sandra <sandramilenaromo@gmail.com>
2021-07-31 16:56:08 -0500

    add test modification

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -11,8 +11,8 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-			   String message="default message cambio test devops";
-			   Assert.assertNotNull(message);
+			   String meessage="default message cambio test devops";
+			   Assert.assertNotNull(meessage);
 	}
 
 }
